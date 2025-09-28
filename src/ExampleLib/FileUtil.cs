caed478fe35d270cd385755bf1cca396010a7e44
wander122156 <sov48656@mail.ru>
2025-09-28 16:11:19 +0300

    добавил 1.1D

--- src/ExampleLib/FileUtil.cs
+++ src/ExampleLib/FileUtil.cs
@@ -8,6 +8,28 @@ public static class FileUtil
     /// Сортирует строки в указанном файле.
     /// Перезаписывает файл, но не атомарно: ошибка ввода-вывода при записи приведёт к потере данных.
     /// </summary>
+    public static void AddLineNumbers(string path)
+    {
+        int lineNum = 1;
+        List<string> lines = File.ReadLines(path, Encoding.UTF8).ToList();
+
+        using FileStream file = File.Open(path, FileMode.Truncate, FileAccess.Write);
+        for (int i = 0, iMax = lines.Count; i < iMax; ++i)
+        {
+            byte[] testBytes = Encoding.UTF8.GetBytes(lines[i]);
+            byte[] numberBytes = Encoding.UTF8.GetBytes($"{lineNum}. ");
+            file.Write(numberBytes);
+            file.Write(testBytes);
+
+            if (i != iMax - 1)
+            {
+                file.Write("\n"u8);
+            }
+
+            lineNum++;
+        }
+    }
+
     public static void SortFileLines(string path)
     {
         // Читаем и сортируем строки файла.
--- tests/ExampleLib.UnitTests/FileUtilTests.cs
+++ tests/ExampleLib.UnitTests/FileUtilTests.cs
@@ -49,4 +49,54 @@ public class FileUtilTests
         string actual = File.ReadAllText(file.Path);
         Assert.Equal("", actual);
     }
+
+    /// <summary>
+    /// 1-лаба AddLineNumbers тесты.
+    /// </summary>
+    [Fact]
+    public void CanAddLineNumber()
+    {
+        const string unnumbered = """
+                                Играют волны — ветер свищет,
+                                И мачта гнется и скрыпит…
+                                Увы! он счастия не ищет
+                                И не от счастия бежит!
+                                """;
+        const string numbered = """
+                                1. Играют волны — ветер свищет,
+                                2. И мачта гнется и скрыпит…
+                                3. Увы! он счастия не ищет
+                                4. И не от счастия бежит!
+                                """;
+
+        using TempFile file = TempFile.Create(unnumbered);
+        FileUtil.AddLineNumbers(file.Path);
+
+        string actual = File.ReadAllText(file.Path);
+        Assert.Equal(numbered.Replace("\r\n", "\n"), actual);
+
+    }
+
+    [Fact]
+    public void CanLineNumberOneLineFile()
+    {
+
+        using TempFile file = TempFile.Create("Играют волны — ветер свищет,");
+        FileUtil.AddLineNumbers(file.Path);
+
+        string actual = File.ReadAllText(file.Path);
+        Assert.Equal("1. Играют волны — ветер свищет,", actual);
+
+    }
+
+    [Fact]
+    public void CanLineNumberEmptyFile()
+    {
+        using TempFile file = TempFile.Create("");
+
+        FileUtil.AddLineNumbers(file.Path);
+
+        string actual = File.ReadAllText(file.Path);
+        Assert.Equal("", actual);
+    }
 }
